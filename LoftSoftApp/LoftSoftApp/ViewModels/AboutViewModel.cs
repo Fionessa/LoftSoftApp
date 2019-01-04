d1bd10b6c2ac848a8ac79c59fbed8f4aaeee7d61
Pascal Pfister <pascal.pfister@loftsoft.ch>
2019-01-04 17:36:54 +0100

    Versuche mit Xamarin Essentials

--- LoftSoftApp/LoftSoftApp/ViewModels/AboutViewModel.cs
+++ LoftSoftApp/LoftSoftApp/ViewModels/AboutViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Input;
-
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace LoftSoftApp.ViewModels
 {
@@ -12,8 +12,77 @@ namespace LoftSoftApp.ViewModels
             Title = "Über Loftsoft";
 
             OpenWebCommand = new Command(() => Device.OpenUri(new Uri("http://loftsoft.ch")));
+            //BatteryTestCommand = new Command(() => this.BatteryTest());
         }
 
         public ICommand OpenWebCommand { get; }
+
+
+        //private ICommand _batteryTestCommand;
+
+        //public ICommand BatteryTestCommand {
+        //    get
+        //    {
+        //        if (_batteryTestCommand == null)
+        //        {
+        //            _batteryTestCommand = new RelayCommand(
+        //                param => this.BatteryTest()
+        //            );
+        //        }
+        //        return _saveCommand;
+        //    }
+        //}
+        
+
+        public void BatteryTest()
+        {
+            var level = Battery.ChargeLevel; // returns 0.0 to 1.0 or 1.0 when on AC or no battery.
+
+            var state = Battery.State;
+
+            switch (state)
+            {
+                case BatteryState.Charging:
+                    // Currently charging
+                    break;
+                case BatteryState.Full:
+                    // Battery is full
+                    break;
+                case BatteryState.Discharging:
+                case BatteryState.NotCharging:
+                    // Currently discharging battery or not being charged
+                    break;
+                case BatteryState.NotPresent:
+                // Battery doesn't exist in device (desktop computer)
+                case BatteryState.Unknown:
+                    // Unable to detect battery state
+                    break;
+            }
+
+            var source = Battery.PowerSource;
+
+            switch (source)
+            {
+                case BatteryPowerSource.Battery:
+                    // Being powered by the battery
+                    break;
+                case BatteryPowerSource.AC:
+                    // Being powered by A/C unit
+                    break;
+                case BatteryPowerSource.Usb:
+                    // Being powered by USB cable
+                    break;
+                case BatteryPowerSource.Wireless:
+                    // Powered via wireless charging
+                    break;
+                case BatteryPowerSource.Unknown:
+                    // Unable to detect power source
+                    break;
+            }
+
+
+        }
+
+
     }
 }
--- LoftSoftApp/LoftSoftApp/Views/AboutPage.xaml.cs
+++ LoftSoftApp/LoftSoftApp/Views/AboutPage.xaml.cs
@@ -2,6 +2,7 @@
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace LoftSoftApp.Views
 {
@@ -12,5 +13,95 @@ namespace LoftSoftApp.Views
         {
             InitializeComponent();
         }
+
+        private void CheckBattery_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "You clicked me!";
+
+            BatteryTest();
+        }
+
+        private void CheckBattery_Pressed(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "I was just pressed!";
+        }
+
+        public async void BatteryTest()
+        {
+            var level = Battery.ChargeLevel; // returns 0.0 to 1.0 or 1.0 when on AC or no battery.
+
+            var state = Battery.State;
+
+            switch (state)
+            {
+                case BatteryState.Charging:
+                    // Currently charging
+                    break;
+                case BatteryState.Full:
+                    // Battery is full
+                    break;
+                case BatteryState.Discharging:
+                case BatteryState.NotCharging:
+                    // Currently discharging battery or not being charged
+                    break;
+                case BatteryState.NotPresent:
+                // Battery doesn't exist in device (desktop computer)
+                case BatteryState.Unknown:
+                    // Unable to detect battery state
+                    break;
+            }
+
+            var source = Battery.PowerSource;
+
+            switch (source)
+            {
+                case BatteryPowerSource.Battery:
+                    // Being powered by the battery
+                    break;
+                case BatteryPowerSource.AC:
+                    // Being powered by A/C unit
+                    break;
+                case BatteryPowerSource.Usb:
+                    // Being powered by USB cable
+                    break;
+                case BatteryPowerSource.Wireless:
+                    // Powered via wireless charging
+                    break;
+                case BatteryPowerSource.Unknown:
+                    // Unable to detect power source
+                    break;
+            }
+
+            // Device Model (SMG-950U, iPhone10,6)
+            var device = DeviceInfo.Model;
+
+            // Manufacturer (Samsung)
+            var manufacturer = DeviceInfo.Manufacturer;
+
+            // Device Name (Motz's iPhone)
+            var deviceName = DeviceInfo.Name;
+
+            // Operating System Version Number (7.0)
+            var version = DeviceInfo.VersionString;
+
+            // Platform (Android)
+            var platform = DeviceInfo.Platform;
+
+            // Idiom (Phone)
+            var idiom = DeviceInfo.Idiom;
+
+            // Device Type (Physical)
+            var deviceType = DeviceInfo.DeviceType;
+
+            // @47.4771515,8.3094978,19
+
+            var location = new Location(47.4771515, 8.3094978);
+            var options = new MapLaunchOptions { Name = "Bahnhofstrasse 31, 5400 Baden" };
+
+            await Map.OpenAsync(location, options);
+
+        }
+
+
     }
 }
